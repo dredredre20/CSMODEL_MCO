85fc59124aa6fef98eaf9aac5906613e836737be
Andre <andrechu17@gmail.com>
2025-07-25 20:11:52 +0800

    New Data Mining Files

--- rule_miner.py
+++ rule_miner.py
@@ -5,7 +5,7 @@ class RuleMiner(object):
 
     def __init__(self, support_t, confidence_t):
         self.support_t = support_t
-        self.confidence_t = confidence_t
+        self.confidence_t = confidence_t 
 
     def get_support(self, data, itemset):
         curr_itemset = data[itemset].all(axis=1)
@@ -13,83 +13,74 @@ class RuleMiner(object):
         return support
 
     def merge_itemsets(self, itemsets):
+        if not itemsets:
+            return []
 
-        new_itemsets = []
+        new_itemsets = set()
+        k = len(itemsets[0])
 
-        cur_num_items = len(itemsets[0])
+        # Corrected loop: starts from 0 instead of i+1
+        for i in range(len(itemsets)):
+            for j in range(i+1, len(itemsets)):
+                combined_itemset = list(set(itemsets[i]) | set(itemsets[j]))
+                combined_itemset.sort()
 
-        if cur_num_items == 1:
-            for i in range(len(itemsets)):
-                for j in range(i + 1, len(itemsets)):
-                    new_itemsets.append(list(set(itemsets[i]) | set(itemsets[j])))
+                if len(combined_itemset) == k + 1:
+                    new_itemsets.add(tuple(combined_itemset))
 
-        else:
-            for i in range(len(itemsets)):
-                for j in range(i + 1, len(itemsets)):
-                    combined_list = list(set(itemsets[i]) | set(itemsets[j]))
-                    combined_list.sort()
-                    if len(combined_list) == cur_num_items + 1 and combined_list not in new_itemsets:
-                        new_itemsets.append(combined_list)
+        return [list(itemset) for itemset in new_itemsets]
 
-        return new_itemsets
+
 
     def get_rules(self, itemset):
-
-        combinations = itertools.combinations(itemset, len(itemset) - 1)
-        combinations = [list(combination) for combination in combinations]
-
+    
         rules = []
-        for combination in combinations:
-            diff = set(itemset) - set(combination)
-            rules.append([combination, list(diff)])
-            rules.append([list(diff), combination])
-
+        for i in range(1, len(itemset)):
+            for antecedent in itertools.combinations(itemset, i):
+                antecedent = list(antecedent)
+                consequent = list(set(itemset) - set(antecedent))
+                rules.append([antecedent, consequent])
         return rules
+
 
     def get_frequent_itemsets(self, data):
     
         itemsets = [[i] for i in data.columns]
-        old_itemsets = []
-        flag = True
+        freq_itemsets = []
 
-        while flag:
+        while itemsets:
             new_itemsets = []
             for itemset in itemsets:
-
-                if (self.get_support(data, itemset) >= self.support_t):
+                
+                if self.get_support(data, itemset) >= self.support_t:
+                    freq_itemsets.append(itemset)
                     new_itemsets.append(itemset)
 
-            if len(new_itemsets) != 0:
-                old_itemsets = new_itemsets
-                itemsets = self.merge_itemsets(new_itemsets)
-            else:
-                flag = False
-                itemsets = old_itemsets
-
-        return itemsets
-
+            itemsets = self.merge_itemsets(new_itemsets) if new_itemsets else []
+        
+        return freq_itemsets
 
 
     def get_confidence(self, data, rule):
+        
         X = rule[0]
         Y = rule[1]
 
-        support = self.get_support(data, X)
-        concat_list = X + Y
-        support_concat = self.get_support(data, concat_list)
+        support_X = self.get_support(data, X)
+        support_XY = self.get_support(data, X + Y)
 
-        if (support_concat == 0  or support == 0):
+        if support_X == 0:
             return 0.0
         else:
-            confidence = support_concat / support
+            confidence = support_XY / support_X
             return confidence
 
-        
 
     def get_association_rules(self, data):
 
         itemsets = self.get_frequent_itemsets(data)
         rules = []
+        
         for itemset in itemsets:
 
             if len(itemset) > 1:
